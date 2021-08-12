242b07580dd4f3a86a9b22052b7309d3e1e99d46
papercrane1001 <papercrane1001@gmail.com>
2021-08-12 14:55:51 -0700

    At least I added the loadout class.

---
+++ Source/WeaponsTabReborn/WeaponsTabReborn/Loadout.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using Verse;
+
+using RimWorld;
+
+
+namespace WeaponsTabReborn
+{
+    public class Loadout : IExposable, ILoadReferenceable
+    {
+        public int uniqueID;
+        public string label;
+        public List<ThingFilter> filters = new List<ThingFilter>();
+
+        public static readonly Regex ValidNameRegex = new Regex("^[\\p{L}0-9 '\\-]*$");
+
+        public Loadout()
+        {
+
+        }
+
+        public Loadout(int _uniqueID, string _label)
+        {
+            uniqueID = _uniqueID;
+            label = _label;
+        }
+
+        public void ExposeData()
+        {
+            Scribe_Values.Look(ref uniqueID, "uniqueID", 0);
+            Scribe_Values.Look(ref label, "label");
+            Scribe_Deep.Look(ref filters, "filters");
+        }
+
+        public string GetUniqueLoadID()
+        {
+            return "Loadout_" + label + uniqueID.ToString();
+        }
+    }
+}

6d3f1b682b3fce64b9664776bc5e7d9d380f689c
papercrane1001 <papercrane1001@gmail.com>
2021-08-13 12:32:46 -0700

    Started prep for alternate UI

--- Source/WeaponsTabReborn/WeaponsTabReborn/Loadout.cs
+++ Source/WeaponsTabReborn/WeaponsTabReborn/Loadout.cs
@@ -16,7 +16,9 @@ namespace WeaponsTabReborn
         public int uniqueID;
         public string label;
         public ThingFilter filter = new ThingFilter();
-        public List<ThingFilter> filters = new List<ThingFilter>();
+        public List<ThingFilter> allFilters = new List<ThingFilter>(); 
+        public List<ThingFilter> oneFilters = new List<ThingFilter>();
+
 
         public static readonly Regex ValidNameRegex = new Regex("^[\\p{L}0-9 '\\-]*$");
 
@@ -36,7 +38,8 @@ namespace WeaponsTabReborn
             Scribe_Values.Look(ref uniqueID, "uniqueID", 0);
             Scribe_Values.Look(ref label, "label");
             Scribe_Deep.Look(ref filter, "filter");
-            Scribe_Deep.Look(ref filters, "filters");
+            Scribe_Deep.Look(ref allFilters, "allFilters");
+            Scribe_Deep.Look(ref oneFilters, "oneFilters");
         }
 
         public string GetUniqueLoadID()
